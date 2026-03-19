db1b5c25a5119af81d2c8603549756382798c592
Stefan Asemota <stephan.asemota@gmail.com>
2026-03-19 20:27:15 +0000

    favicon are places in public/favicon. please update the apps favicon mak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,14 @@ export const metadata: Metadata = {
   title: 'SABI AI - Knowledge at Work',
   description:
     'Niche Productivity & AI Solutions.',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any', type: 'image/x-icon' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
